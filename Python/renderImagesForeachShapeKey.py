1e7973e182eb18795b6899e50a74ba4d7f38c430
mi-ni-mi-mi <79532972+mi-ni-mi-mi@users.noreply.github.com>
2022-09-04 22:29:39 +0900

    Update renderImagesForeachShapeKey.py

--- Python/renderImagesForeachShapeKey.py
+++ Python/renderImagesForeachShapeKey.py
@@ -1,17 +1,17 @@
 import bpy
 
-bpy.context.scene.render.image_settings.file_format = 'PNG'
-
+dir = bpy.context.scene.render.filepath
 #keys = bpy.data.shape_keys['Key'].key_blocks #Key名を指定する場合
 keys = bpy.context.active_object.data.shape_keys.key_blocks #オブジェクトを指定する場合
 
 for i in range(len(keys)):
-    #filename = 'D:\\xxx\\xxx\\xxx\\' + keys[i].name + '.png' #日本語だとエラー
-    filename = 'D:\\' + str(i) + '.png'
+    #filename = keys[i].name + '.png' #日本語だとエラー
+    filename = str(i) + '.png'
+    path = dir + filename
     if i > 0:
         keys[i-1].value = 0
     keys[i].value = 1
     bpy.ops.render.render()
-    bpy.data.images['Render Result'].save_render(filepath = filename)
+    bpy.data.images['Render Result'].save_render(filepath = path)
     
-keys[len(keys)].value = 0
+keys[len(keys)-1].value = 0
