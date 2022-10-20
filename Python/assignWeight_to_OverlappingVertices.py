359c7c1eda54072d6a78707dbe539cd54ac6295e
mi-ni-mi-mi <79532972+mi-ni-mi-mi@users.noreply.github.com>
2022-10-21 05:29:44 +0900

    Update assignWeight_to_OverlappingVertices.py

--- Python/assignWeight_to_OverlappingVertices.py
+++ Python/assignWeight_to_OverlappingVertices.py
@@ -2,19 +2,20 @@ import bpy
 
 def assignWeight_to_OverlappingVertices():
     
-    if bpy.context.selected_objects[0].type == 'Mesh':
-        arm = bpy.context.selected_objects[1]
-    else:
+    if bpy.context.selected_objects[0].type == 'Armature':
         obj = bpy.context.selected_objects[1]
         arm = bpy.context.selected_objects[0]
-        
+    else:
+        obj = bpy.context.selected_objects[0]
+        arm = bpy.context.selected_objects[1]
+    
     boneNames = obj.vertex_groups.keys()
     
     for vert in obj.data.vertices:
         for boneName in boneNames:
             tail_co = arm.data.bones[boneName].tail_local
             length = (vert.co - tail_co).length
-            if length < 0.001:
+            if length < 0.00001:
                 vertGroup = obj.vertex_groups[boneName]
                 vertGroup.add([vert.index], 1.0, 'REPLACE')
 
