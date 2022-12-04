348bbaa50a1a290d0a35ed97ef448e454d7d9682
mi-ni-mi-mi <79532972+mi-ni-mi-mi@users.noreply.github.com>
2022-12-04 21:35:25 +0900

    Update and rename assignWeights_to_OverlappingVertices.py to assign_weights_to_overlapping_vertices.py

--- Python/assign_weights_to_overlapping_vertices.py
+++ Python/assign_weights_to_overlapping_vertices.py
@@ -1,6 +1,6 @@
 import bpy
 
-def assignWeights_to_OverlappingVertices():
+def assign_weight_to_overlapping_vertices():
     
     if bpy.context.mode != 'OBJECT':
         bpy.ops.object.mode_set(mode='OBJECT')
@@ -12,14 +12,14 @@ def assignWeights_to_OverlappingVertices():
         obj = bpy.context.selected_objects[0]
         arm = bpy.context.selected_objects[1]
     
-    boneNames = obj.vertex_groups.keys()
+    bone_names = obj.vertex_groups.keys()
     
     for vert in obj.data.vertices:
-        for boneName in boneNames:
-            tail_co = arm.data.bones[boneName].tail_local
+        for bone_name in bone_names:
+            tail_co = arm.data.bones[bone_name].tail_local
             length = (vert.co - tail_co).length
-            if length < 0.00001:
-                vertGroup = obj.vertex_groups[boneName]
-                vertGroup.add([vert.index], 1.0, 'REPLACE')
+            if length < 0.0001:
+                vert_group = obj.vertex_groups[bone_name]
+                vert_group.add([vert.index], 1.0, 'REPLACE')
 
-assignWeights_to_OverlappingVertices()
+assign_weight_to_overlapping_vertices()
