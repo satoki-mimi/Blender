7747912f0570ad8f280e20c8faa57c8c83bd18b6
satoki-mimi <79532972+satoki-mimi@users.noreply.github.com>
2022-12-11 23:35:15 +0900

    Update rename_bones_vrm_to_pmx.py

--- Python/rename_bones_vrm_to_pmx.py
+++ Python/rename_bones_vrm_to_pmx.py
@@ -10,66 +10,68 @@ import bpy
 """
 
 def rename_bones():
-  for bone in bpy.context.active_object.data.bones:
-    if bone.name == 'Root': bone.name = 'グルーブ'
-    elif bone.name == 'J_Bip_C_Hips': bone.name = '下半身'
-    elif bone.name == 'J_Bip_C_Spine': bone.name = '上半身'
-    elif bone.name == 'J_Bip_C_Chest': bone.name = '上半身1' #後ほどDissolve
-    elif bone.name == 'J_Bip_C_UpperChest': bone.name = '上半身2'
-    elif bone.name == 'J_Sec_L_Bust1': bone.name = 'おっぱい.L'
-    elif bone.name == 'J_Sec_R_Bust1': bone.name = 'おっぱい.R'
-    elif bone.name == 'J_Sec_L_Bust2': bone.name = 'おっぱい2.L' #基本のボーンにはない
-    elif bone.name == 'J_Sec_R_Bust2': bone.name = 'おっぱい2.R' #基本のボーンにはない
-    elif bone.name == 'J_Bip_C_Neck': bone.name = '首'
-    elif bone.name == 'J_Bip_C_Head': bone.name = '頭'
-    elif bone.name == 'J_Adj_L_FaceEye': bone.name = '目.L'
-    elif bone.name == 'J_Adj_R_FaceEye': bone.name = '目.R'
-    elif bone.name == 'J_Bip_L_Shoulder': bone.name = '肩.L'
-    elif bone.name == 'J_Bip_L_UpperArm': bone.name = '腕.L'
-    elif bone.name == 'J_Bip_L_LowerArm': bone.name = 'ひじ.L'
-    elif bone.name == 'J_Bip_L_Hand': bone.name = '手首.L'
-    elif bone.name == 'J_Bip_L_Thumb1': bone.name = '親指０.L'
-    elif bone.name == 'J_Bip_L_Thumb2': bone.name = '親指１.L'
-    elif bone.name == 'J_Bip_L_Thumb3': bone.name = '親指２.L'
-    elif bone.name == 'J_Bip_L_Index1': bone.name = '人指１.L'
-    elif bone.name == 'J_Bip_L_Index2': bone.name = '人指２.L'
-    elif bone.name == 'J_Bip_L_Index3': bone.name = '人指３.L'
-    elif bone.name == 'J_Bip_L_Middle1': bone.name = '中指１.L'
-    elif bone.name == 'J_Bip_L_Middle2': bone.name = '中指２.L'
-    elif bone.name == 'J_Bip_L_Middle3': bone.name = '中指３.L'
-    elif bone.name == 'J_Bip_L_Ring1': bone.name = '薬指１.L'
-    elif bone.name == 'J_Bip_L_Ring2': bone.name = '薬指２.L'
-    elif bone.name == 'J_Bip_L_Ring3': bone.name = '薬指３.L'
-    elif bone.name == 'J_Bip_L_Little1': bone.name = '小指１.L'
-    elif bone.name == 'J_Bip_L_Little2': bone.name = '小指２.L'
-    elif bone.name == 'J_Bip_L_Little3': bone.name = '小指３.L'
-    elif bone.name == 'J_Bip_R_Shoulder': bone.name = '肩.R'
-    elif bone.name == 'J_Bip_R_UpperArm': bone.name = '腕.R'
-    elif bone.name == 'J_Bip_R_LowerArm': bone.name = 'ひじ.R'
-    elif bone.name == 'J_Bip_R_Hand': bone.name = '手首.R'
-    elif bone.name == 'J_Bip_R_Thumb1': bone.name = '親指０.R'
-    elif bone.name == 'J_Bip_R_Thumb2': bone.name = '親指１.R'
-    elif bone.name == 'J_Bip_R_Thumb3': bone.name = '親指２.R'
-    elif bone.name == 'J_Bip_R_Index1': bone.name = '人指１.R'
-    elif bone.name == 'J_Bip_R_Index2': bone.name = '人指２.R'
-    elif bone.name == 'J_Bip_R_Index3': bone.name = '人指３.R'
-    elif bone.name == 'J_Bip_R_Middle1': bone.name = '中指１.R'
-    elif bone.name == 'J_Bip_R_Middle2': bone.name = '中指２.R'
-    elif bone.name == 'J_Bip_R_Middle3': bone.name = '中指３.R'
-    elif bone.name == 'J_Bip_R_Ring1': bone.name = '薬指１.R'
-    elif bone.name == 'J_Bip_R_Ring2': bone.name = '薬指２.R'
-    elif bone.name == 'J_Bip_R_Ring3': bone.name = '薬指３.R'
-    elif bone.name == 'J_Bip_R_Little1': bone.name = '小指１.R'
-    elif bone.name == 'J_Bip_R_Little2': bone.name = '小指２.R'
-    elif bone.name == 'J_Bip_R_Little3': bone.name = '小指３.R'
-    elif bone.name == 'J_Bip_L_UpperLeg': bone.name = '足.L'
-    elif bone.name == 'J_Bip_L_LowerLeg': bone.name = 'ひざ.L'
-    elif bone.name == 'J_Bip_L_Foot': bone.name = '足首.L'
-    elif bone.name == 'J_Bip_L_ToeBase': bone.name = 'つま先.L'
-    elif bone.name == 'J_Bip_R_UpperLeg': bone.name = '足.R'
-    elif bone.name == 'J_Bip_R_LowerLeg': bone.name = 'ひざ.R'
-    elif bone.name == 'J_Bip_R_Foot': bone.name = '足首.R'
-    elif bone.name == 'J_Bip_R_ToeBase': bone.name = 'つま先.R'
-  return
-
+    dic = {
+        'Root': 'グルーブ',
+        'J_Bip_C_Hips': '下半身',
+        'J_Bip_C_Spine': '上半身',
+        'J_Bip_C_Chest': '上半身1', #MMDの標準ボーンにはない
+        'J_Bip_C_UpperChest': '上半身2',
+        'J_Sec_L_Bust1': 'おっぱい.L',
+        'J_Sec_R_Bust1': 'おっぱい.R',
+        'J_Sec_L_Bust2': 'おっぱい2.L', #MMDの標準ボーンにはない
+        'J_Sec_R_Bust2': 'おっぱい2.R', #MMDの標準ボーンにはない
+        'J_Bip_C_Neck': '首',
+        'J_Bip_C_Head': '頭',
+        'J_Adj_L_FaceEye': '目.L',
+        'J_Adj_R_FaceEye': '目.R',
+        'J_Bip_L_Shoulder': '肩.L',
+        'J_Bip_L_UpperArm': '腕.L',
+        'J_Bip_L_LowerArm': 'ひじ.L',
+        'J_Bip_L_Hand': '手首.L',
+        'J_Bip_L_Thumb1': '親指０.L',
+        'J_Bip_L_Thumb2': '親指１.L',
+        'J_Bip_L_Thumb3': '親指２.L',
+        'J_Bip_L_Index1': '人指１.L',
+        'J_Bip_L_Index2': '人指２.L',
+        'J_Bip_L_Index3': '人指３.L',
+        'J_Bip_L_Middle1': '中指１.L',
+        'J_Bip_L_Middle2': '中指２.L',
+        'J_Bip_L_Middle3': '中指３.L',
+        'J_Bip_L_Ring1': '薬指１.L',
+        'J_Bip_L_Ring2': '薬指２.L',
+        'J_Bip_L_Ring3': '薬指３.L',
+        'J_Bip_L_Little1': '小指１.L',
+        'J_Bip_L_Little2': '小指２.L',
+        'J_Bip_L_Little3': '小指３.L',
+        'J_Bip_R_Shoulder': '肩.R',
+        'J_Bip_R_UpperArm': '腕.R',
+        'J_Bip_R_LowerArm': 'ひじ.R',
+        'J_Bip_R_Hand': '手首.R',
+        'J_Bip_R_Thumb1': '親指０.R',
+        'J_Bip_R_Thumb2': '親指１.R',
+        'J_Bip_R_Thumb3': '親指２.R',
+        'J_Bip_R_Index1': '人指１.R',
+        'J_Bip_R_Index2': '人指２.R',
+        'J_Bip_R_Index3': '人指３.R',
+        'J_Bip_R_Middle1': '中指１.R',
+        'J_Bip_R_Middle2': '中指２.R',
+        'J_Bip_R_Middle3': '中指３.R',
+        'J_Bip_R_Ring1': '薬指１.R',
+        'J_Bip_R_Ring2': '薬指２.R',
+        'J_Bip_R_Ring3': '薬指３.R',
+        'J_Bip_R_Little1': '小指１.R',
+        'J_Bip_R_Little2': '小指２.R',
+        'J_Bip_R_Little3': '小指３.R',
+        'J_Bip_L_UpperLeg': '足.L',
+        'J_Bip_L_LowerLeg': 'ひざ.L',
+        'J_Bip_L_Foot': '足首.L',
+        'J_Bip_L_ToeBase': 'つま先.L',
+        'J_Bip_R_UpperLeg': '足.R',
+        'J_Bip_R_LowerLeg': 'ひざ.R',
+        'J_Bip_R_Foot': '足首.R',
+        'J_Bip_R_ToeBase': 'つま先.R'
+    }
+    for bone in bpy.context.active_object.data.bones:
+        if bone.name in dic:
+            bone.name = dic[bone.name]
 rename_bones()
